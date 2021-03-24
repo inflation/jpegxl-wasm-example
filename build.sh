e1106eab002ff4f5a8c3e3de118daa03272b888f
Inflation <hypernovagama@gmail.com>
2021-03-24 14:01:15 -0400

    Bump version to 0.3.5

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -e
+
 emcc main.c -O3 -flto -o jpegxl.js \
     -I../jpeg-xl/build-wasm32/install/include \
     -L../jpeg-xl/build-wasm32/install/lib \
-    -ljxl -ljxl_threads -lhwy -lskcms \
+    -L../jpeg-xl/build-wasm32/third_party \
+    -L../jpeg-xl/build-wasm32/third_party/brotli \
+    -ljxl -ljxl_threads -lhwy -lskcms -lbrotlicommon-static -lbrotlidec-static -lbrotlienc-static \
     -sEXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -sALLOW_MEMORY_GROWTH=1 \
     --no-entry
 wasm-opt -o jpegxl.wasm -O4 --disable-simd jpegxl.wasm
